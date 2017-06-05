3058773dcfc0a8cc608ba576cdd69f56fd6f3945
github username <github email address>
2017-06-05 11:36:48 -0400

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 
 import RegistrationForm from './components/RegistrationForm';
 import Webpage from './components/Webpage';
+import FillerText from './components/FillerText';
 
 ReactDOM.render(
   (
     <div>
-      <RegistrationForm />
+      <FillerText />
       <Webpage />
+      <RegistrationForm />
+
+
     </div>
   ),
   document.getElementById('main')
